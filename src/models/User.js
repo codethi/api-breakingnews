3113a2d83a3f63aa897ac117792d46e0d5b0c3af
codethi <docencia.thiago@gmail.com>
2022-05-20 19:32:36 -0300

    new attribute user

--- src/models/User.js
+++ src/models/User.js
@@ -26,6 +26,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  background: {
+    type: String,
+    required: true,
+  },
 });
 
 UserSchema.pre("save", async function (next) {
