8f4cf6c229b09500a8a80f104171f512ffc7bc9b
codethi <docencia.thiago@gmail.com>
2023-05-03 20:15:26 -0300

    refact: config database

--- src/database/database.js
+++ src/database/database.js
@@ -1,8 +1,6 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const connectDatabase = () => {
-  console.log("Wait connecting to the database...");
-
+function connectDatabase() {
   mongoose
     .connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
@@ -10,6 +8,6 @@ const connectDatabase = () => {
     })
     .then(() => console.log("MongoDB Atlas Connected!"))
     .catch((err) => console.log(`Error connecting to MongoDB Atlas: ${err}`));
-};
+}
 
-module.exports = connectDatabase;
+export default connectDatabase;
